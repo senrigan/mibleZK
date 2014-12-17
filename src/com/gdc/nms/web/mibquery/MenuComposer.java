4b09bd31530cb3f60221baf75479558ac0415720
gilbertoC <gilberto_1_0@hotmail.com>
2014-12-17 15:11:11 -0600

    Adding new files

--- src/com/gdc/nms/web/mibquery/MenuComposer.java
+++ src/com/gdc/nms/web/mibquery/MenuComposer.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 
 import org.zkoss.zhtml.Div;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zkmax.zul.Nav;
 import org.zkoss.zkmax.zul.Navbar;
 import org.zkoss.zul.Include;
 import org.zkoss.zul.Label;
+import org.zkoss.zul.Window;
 
 
 public class MenuComposer extends SelectorComposer<Component>{
@@ -19,11 +22,15 @@ public class MenuComposer extends SelectorComposer<Component>{
    
     private static final long serialVersionUID = -2938915709081840018L;
  
+    @Wire("#windowModal")
+    	private Window windowModal;
     @Wire("#menu")
     	private Div menu; 
     
     @Wire("#menuBar")
     	private Navbar menuBar;
+    @Wire("#menuPoller")
+    	private Nav menuPoller;
     
     @Wire("#middle")
     	private Div middle; 
@@ -36,6 +43,33 @@ public class MenuComposer extends SelectorComposer<Component>{
 		String menuName=menuBar.getSelectedItem().getLabel();
 		Clients.showNotification(menuName);
 		changeContainerPage(menuName);
+    	}
+    @Listen("onOpen=#menuPoller")
+    	public void pollerOption() {
+		Clients.showNotification("algo se abrio",true);
+		String menuName=menuPoller.getNavbar().getSelectedItem().getLabel();
+		Clients.showNotification(menuName);
+		
+    	}
+    @Listen("onClick=#menuPoller > navitem")
+	public void pollerOption3() {
+		Clients.showNotification("algo se clikeo",true);
+		String menuName=menuPoller.getNavbar().getSelectedItem().getLabel();
+		Clients.showNotification(menuName);
+	}
+    @Listen("onClick=Nav > Navitem")
+	public void pollerOption2() {
+		Clients.showNotification("algo se dropeo",true);
+		String menuName=menuPoller.getNavbar().getSelectedItem().getLabel();
+		
+		Clients.showNotification(menuName);
+		 Window window = (Window)Executions.createComponents(
+		                "modal.zul", null, null);
+		        window.doModal();
+	}
+    @Listen("onMouseOut=#windowModal")
+    	public void exitWindoModal() {
+		Clients.showNotification("se salio");
     	}
     
     public void changeContainerPage(String dirPage){
