772a70b372ab1043275bbed728f32e2f95ecdb0f
gilbertoC <gilberto_1_0@hotmail.com>
2014-12-23 14:57:02 -0600

    1456

--- src/com/gdc/nms/web/mibquery/LoadMibs.java
+++ src/com/gdc/nms/web/mibquery/LoadMibs.java
@@ -3,6 +3,7 @@ package com.gdc.nms.web.mibquery;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -13,13 +14,14 @@ import net.percederberg.mibble.MibLoaderLog;
 import net.percederberg.mibble.MibSymbol;
 import net.percederberg.mibble.MibValue;
 import net.percederberg.mibble.MibValueSymbol;
+import net.percederberg.mibble.snmp.SnmpObjectType;
 import net.percederberg.mibble.value.ObjectIdentifierValue;
 
 public class LoadMibs {
 	private static LoadMibs instance = null;
 	private static HashMap<String, MibValueSymbol> nodesTable;
 	private MibLoader loader;
-	private String dirPath = "/home/gil/Documentos/datamib";
+	private String dirPath = "/home/gilberto/Documentos/old data/docuemntos/datamib/";
 	private ArrayList<MibInfo> loadedMib;
 	private ArrayList<MibInfo> noLoadedMib;
 
@@ -209,6 +211,80 @@ public class LoadMibs {
 			}
 		}
 		return nodes;
+	}
+	
+	public static void main(String [] args) {
+	    LoadMibs l=LoadMibs.getInstance();
+	    MibLoader miblod=l.getLoader();
+	    Mib mibs[]=miblod.getAllMibs();
+	    MibSymbol symbol;
+	    Iterator<?> iter;
+	    int nummibns=0;
+	    for(Mib aux:mibs) {
+		nummibns++;
+		iter = aux.getAllSymbols().iterator();
+		while (iter.hasNext()) {
+			MibValue value;
+			ObjectIdentifierValue oid;
+			MibValueSymbol oidTable;
+			symbol = (MibSymbol) iter.next();
+			if (symbol instanceof MibValueSymbol) {
+				value = ((MibValueSymbol) symbol).getValue();
+				oidTable = (MibValueSymbol) symbol;
+				MibValueSymbol symbol2=oidTable;
+				 if (symbol != null && symbol2.getType() instanceof SnmpObjectType) {
+				           SnmpObjectType snmp=(SnmpObjectType) symbol2.getType();
+				           System.out.println("snmp nam"+snmp.getName());
+				           System.out.println("snmp acces"+snmp.getAccess());
+				           System.out.println("snmp augment"+snmp.getAugments());
+				           System.out.println("snmp commet"+snmp.getComment());
+				           System.out.println("snmp descriop"+snmp.getDescription());
+				           System.out.println("snmp reference"+snmp.getReference());
+				           System.out.println("snmp unit"+snmp.getUnits());
+				           System.out.println("snmp defaultvalue"+snmp.getDefaultValue());
+				           System.out.println("snmp index"+snmp.getIndex());
+				           System.out.println("snmp status"+snmp.getStatus());
+				           System.out.println("snmp syntaxis"+snmp.getSyntax());
+
+
+
+
+
+
+
+
+				        } else {
+				            System.out.println("algo paso");
+				        }
+				if (value instanceof ObjectIdentifierValue) {
+					String oidString;
+					oid = (ObjectIdentifierValue) value;
+					oidString = "" + oid;
+					//System.out.println("value oid"+oid.getValue());//valor final del oid
+					System.out.println("type oid"+oid.getSymbol());
+
+					System.out.println("table"+oid.getSymbol().isTable());
+					System.out.println("escalar"+oid.getSymbol().isScalar());
+					System.out.println("column"+oid.getSymbol().isTableColumn());
+					System.out.println("row"+oid.getSymbol().isTableRow());
+
+					try {
+					    System.out.println("typeV oid"+oid.getSymbol().getType().getTag().getValue());
+					    System.out.println("typeN oid"+oid.getSymbol().getType().getTag().getNext());
+
+					}catch(Exception ex) {
+					    System.out.println("error");
+
+					}
+					
+					//System.out.println("type oid"+oid.getSymbol().getType().getTag().get);
+					//System.out.println("symbol oid"+Arrays.toString(oid.getSymbol().getChildren()));
+					//System.out.println("refer oid"+oid.getReferenceSymbol());
+				}
+			}
+		}
+	    }
+	    System.out.println("total mibs"+ nummibns);
 	}
 
 }
